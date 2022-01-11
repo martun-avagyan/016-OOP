bd28d045d888c4a2b845d9849556e0dec7ed003d
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-01-11 21:30:01 +0400

    fix: Added tests and logs

--- 02account.js
+++ 02account.js
@@ -52,12 +52,21 @@ class Account {
       return this._balance;
     }
   }
-  static identifyAccounts(firstAccount, secondAccount) {}
+  static identifyAccounts(firstAccount, secondAccount) {
+    if (firstAccount === secondAccount) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   static incrementId() {
     if (!this.id) this.id = 1;
     else this.id++;
     return this.id;
+  }
+  toString() {
+    return `${this._name}'s account balance is ${this._balance}`;
   }
 }
 
@@ -68,3 +77,17 @@ const cardAccount = new Account("Card Account", 2000);
 console.log(savingsAccount);
 console.log(currentAccount);
 console.log(cardAccount);
+
+console.log(savingsAccount); // Account { id: 0, _name: 'Saving account',_balance: 2000 }
+console.log(cardAccount); // Account { id: 1, _name: 'Card account', _balance:1000 }
+console.log(savingsAccount.balance); // 2000
+console.log(savingsAccount.credit(400)); // 2400
+console.log(savingsAccount.balance); // 2400
+console.log(savingsAccount.debit(9000)); //6600
+console.log(savingsAccount.transferTo(cardAccount, 1000)); // 1400
+console.log(savingsAccount.balance); // 1400
+console.log(savingsAccount.balance); // 2000
+let anotherAccount = savingsAccount;
+console.log(Account.identifyAccounts(savingsAccount, anotherAccount)); // true
+console.log(Account.identifyAccounts(savingsAccount, cardAccount)); // false
+console.log(savingsAccount.toString()); // Saving account's account balance is $1400.
